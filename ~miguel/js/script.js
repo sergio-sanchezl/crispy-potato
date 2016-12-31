f76edbb35a10dc7fa4a1afc9300fff3eb0d4d903
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2016-12-31 17:35:46 +0100

    CSS rules distributed between 'layout' and 'theme'

--- ~miguel/js/script.js
+++ ~miguel/js/script.js
@@ -1,32 +1,56 @@
+/**
+ * Variable para saber si se trata de un dispositivo
+ * con pantalla táctil o no.
+ */
+var pantallaTactil = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+
+/**
+ * Despliega el menú de secciones si estaba oculto,
+ * o lo oculta si estaba desplegado.
+ */
+function cambiarSecciones () {
+
+
+	var oculto = $('#secciones').data ('oculto');
+
+	if (oculto) {
+
+		$('#secciones').animate ({left: '-1px'}, 500);
+		$('#boton_secc').animate ({left: '160px'}, 550, function () {
+			$('#boton_secc').addClass ('desplegado');
+		});
+	} else {
+
+		$('#secciones').animate ({left: '-250px'}, 500);
+		$('#boton_secc').animate ({left: '-60px'}, 400, function () {
+			$('#boton_secc').removeClass ('desplegado');
+		});
+	}
+
+	$('#secciones').data ('oculto', !oculto);
+}
+
 /* Despliega los paneles laterales para los móviles (si están presentes) */
 $(document).ready (function () {
 
 	/* El panel de la izquierda con las secciones comienza oculto */
 	$('#secciones').data ('oculto', 'true');
 
+	/* Botón para las secciones (panel oculto a la izquierda) */
 	$('#boton_secc').click (function () {
 
-		var oculto = $('#secciones').data ('oculto');
-
-		if (oculto) {
-
-			$('#secciones').animate ({left: '-1px'}, 500);
-			$('#boton_secc').animate ({left: '160px'}, 550, function () {
-				$('#boton_secc').addClass ('desplegado');
-			});
-		} else {
-
-			$('#secciones').animate ({left: '-250px'}, 500);
-			$('#boton_secc').animate ({left: '-60px'}, 400, function () {
-				$('#boton_secc').removeClass ('desplegado');
-			});
-		}
-
-		$('#secciones').data ('oculto', !oculto);
+		cambiarSecciones ();
 	});
 
+	/* Botón para el menú */
 	$(".boton_menu").click (function () {
 
 		$(".boton_menu, .menu").toggleClass ("desplegado");
 	});
+/*
+	if (pantallaTactil) {
+
+		$("html").on ("swipe", cambiarSecciones ());
+	}
+*/
 });
