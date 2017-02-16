357d249ba7a8aa5206637be4a15665f9b1dae265
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-02-16 17:07:53 +0100

    Fixed broken links

--- ~miguel/secciones/directorio.php
+++ ~miguel/secciones/directorio.php
@@ -3,10 +3,10 @@
 	$GLOBAL ["contenido_principal"] = "
 		<dl>
 			<h2>Zonas principales</h2>
-			<dt><a href=\"/~miguel/directorio.php\">Directorio</a></dt>
+			<dt><a href=\"/~miguel/secciones/directorio.php\">Directorio</a></dt>
 				<dd>Muestra todas las zonas de la página, con su descripción, y permite acceder a ellas.</dd>
 			<br/>
-			<dt><a href=\"/~miguel/login.php\">Acceso</a></dt>
+			<dt><a href=\"/~miguel/cuentas/login.php\">Acceso</a></dt>
 				<dd>Sección para entrar a una cuenta y poder acceeder a algunas secciones que requieren identificación (como la de escribir artículos)</dd>
 
 			<h2>Páginas externas</h2>
