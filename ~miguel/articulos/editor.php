b241f536bf69c32544419158ff079e12f9338222
Foo-Manroot <miguel.garciamartin@edu.uah.es>
2017-06-28 19:46:40 +0200

    Added support to edit an article

--- ~miguel/articulos/editor.php
+++ ~miguel/articulos/editor.php
@@ -1,18 +1,6 @@
 <?php
 	/* Carga el controlador para la base de datos */
-	include $_SERVER ['DOCUMENT_ROOT'] . "/lib/db.php";
-
-	$html_editor = "
-		<div id='editor_titulo'>
-			Título: <input type='text' value='Título'>
-		</div>
-		<div id='editor_categ'>
-			Categoría: <input type='text' value='artículo'>
-		</div>
-		<form method='POST' action='./editor.php'>
-			<textarea name='editor' id='editor'></textarea>
-		</form>
-		<script src='/~miguel/js/editor.js'></script>";
+	include $_SERVER ["DOCUMENT_ROOT"] . "/lib/db.php";
 
 	/* Si es necesario, comienza la sesión */
 	if (session_status () == PHP_SESSION_NONE)
@@ -30,34 +18,73 @@
 	}
 	else
 	{
-//		/* Si se recibe algo por POST es que se ha pulsado en 'guardar' y hay
-//		 que actualizar la base de datos */
-//		if (!empty ($_POST ["editor"]))
-//		{
-//			/* Añade el texto en el editor para no perder el progreso */
-//			$html_editor .= $_POST ["editor"];
-//		}
-
-		$GLOBALS ["contenido_principal"] = $html_editor;
-
-		/* Añade valores a la sesión para saber el artículo que se está
-		editando */
-		if (!isset ($_SESSION ["id_articulo"]))
-		{
-			$_SESSION ["id_articulo"]
-				= buscar_id_libre_art ($_SESSION ["usuario"]);
-
-			$_SESSION ["uid_articulo"] = $_SESSION ["usuario"];
-
-		}
-
 		/* Añade el código para el editor en la cabecera del HTML */
 		$GLOBALS ["incluir_head"] = "<script src=\""
 						. "/tinymce/js/tinymce/tinymce.min.js\">"
 						. "</script>\n";
+
+		/* Si hay parámetros en la petición GET, comprueba si se trata de un
+		artículo que hay que editar, o de uno que hay que crear */
+		if (!empty ($_GET ["uid"])
+		    && !empty ($_GET ["id_art"])
+		)
+		{
+			$tupla = obtener_articulo ($_GET ["id_art"], $_GET ["uid"]);
+
+			$categoria = $tupla ["categoria"];
+			$titulo = $tupla ["titulo"];
+
+			$texto = $tupla ["texto"];
+			$texto = str_replace (chr (10), "", $texto);
+			$texto = str_replace (chr (13), "", $texto);
+			$texto = str_ireplace ('<','&#x200B;<',$texto);
+
+			$html_editor = "
+				<div id='editor_titulo'>
+					Título: <input type='text' value='$titulo'>
+				</div>
+				<div id='editor_categ'>
+					Categoría: <input type='text' value='$categoria'>
+				</div>
+				<form method='POST' action='./editor.php'>
+					<textarea name='editor' id='editor'>"
+					. $texto
+					. "</textarea>
+				</form>
+				<script src='/~miguel/js/editor.js'></script>
+			";
+
+		}
+		else
+		{
+			/* Añade valores a la sesión para saber el artículo que se está
+			editando */
+			if (!isset ($_SESSION ["id_articulo"]))
+			{
+				$_SESSION ["id_articulo"]
+					= buscar_id_libre_art ($_SESSION ["usuario"]);
+
+				$_SESSION ["uid_articulo"] = $_SESSION ["usuario"];
+
+			}
+
+			$html_editor = "
+				<div id='editor_titulo'>
+					Título: <input type='text' value='Título'>
+				</div>
+				<div id='editor_categ'>
+					Categoría: <input type='text' value='artículo'>
+				</div>
+				<form method='POST' action='./editor.php'>
+					<textarea name='editor' id='editor'></textarea>
+				</form>
+				<script src='/~miguel/js/editor.js'></script>";
+		}
+
+		$GLOBALS ["contenido_principal"] = $html_editor;
 	}
 
 	/* Carga la plantilla */
-	include $_SERVER ['DOCUMENT_ROOT'] . "/plantillas/miguel.php";
+	include $_SERVER ["DOCUMENT_ROOT"] . "/plantillas/miguel.php";
 
 ?>
